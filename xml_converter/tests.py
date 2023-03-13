1f27da4c0c23eed1e7248bd63895ba13d2f2fb8f
Samuel Sampaio <samukasmk@gmail.com>
2023-03-13 00:47:59 -0300

    [feature/tdd-new-cases] New tests for ignore attributes case

--- xml_converter/tests.py
+++ xml_converter/tests.py
@@ -12,6 +12,7 @@ class XMLConversionTestCase(TestCase):
     def setUp(self):
         self.client = Client()
 
+    # Cases test: empty.xml
     def test_logic_xml2dict_convert_empty_document(self):
         with (TEST_DIR / Path('empty.xml')).open() as fp:
             file_content = fp.read()
@@ -40,6 +41,7 @@ class XMLConversionTestCase(TestCase):
                 "Root": "",
             })
 
+    # Cases test: addresses.xml
     def test_logic_xml2dict_convert_addresses(self):
         with (TEST_DIR / Path('addresses.xml')).open() as fp:
             file_content = fp.read()
@@ -120,4 +122,60 @@ class XMLConversionTestCase(TestCase):
                         ]
                     },
                 ],
+            })
+
+    # Cases test: ignore-attributes.xml
+    def test_logic_xml2dict_convert_ignore_attributes_document(self):
+        with (TEST_DIR / Path('ignore-attributes.xml')).open() as fp:
+            file_content = fp.read()
+            response = xml_to_dict(file_content)
+            self.assertEqual(response, {
+                "Root": [
+                    {
+                        "MyDocument": {
+                            "Has": "an element",
+                            "And": {
+                                "SecondElement": "as well"
+                            }
+                        }
+                    }
+                ]
+            })
+
+    def test_connected_convert_ignore_attributes_document(self):
+        with (TEST_DIR / Path('ignore-attributes.xml')).open() as fp:
+            response = self.client.post('/connected/', {
+                'file': fp,
+            })
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.json(), {
+                "Root": [
+                    {
+                        "MyDocument": {
+                            "Has": "an element",
+                            "And": {
+                                "SecondElement": "as well"
+                            }
+                        }
+                    }
+                ]
+            })
+
+    def test_api_convert_ignore_attributes_document(self):
+        with (TEST_DIR / Path('ignore-attributes.xml')).open() as fp:
+            response = self.client.post('/api/converter/convert/', {
+                'file': fp,
+            })
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.json(), {
+                "Root": [
+                    {
+                        "MyDocument": {
+                            "Has": "an element",
+                            "And": {
+                                "SecondElement": "as well"
+                            }
+                        }
+                    }
+                ]
             })
