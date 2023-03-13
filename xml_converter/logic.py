2ad8aa29b3126c54472f32adabf27541cb9d0721
Samuel Sampaio <samukasmk@gmail.com>
2023-03-13 14:05:59 -0300

    [feature/xml-converter-logic] Parsing xml contents

--- xml_converter/logic.py
+++ xml_converter/logic.py
@@ -1,2 +1,9 @@
+import xmltodict
+
+import xml.etree.ElementTree as ET, re
+
+
 def xml_to_dict(xml_content):
-    ...
+    root_element = ET.fromstring(xml_content)
+    response_dict = {root_element.tag: {} if root_element.attrib else ""}
+    return response_dict
